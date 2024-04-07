5eb324eea73f4d83f8f438c45a06fee6ba6a6372
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-08 08:47:40 +0900

    firstadd

---
+++ A/a02_LinearSearch.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, X;
+	cin >> N >> X;
+	for (int i = 0; i < N; i++)
+	{
+		int A;
+		cin >> A;
+		if (A == X)
+		{
+			cout << "Yes" << endl;
+			return 0;
+		}	
+	}
+	cout << "No" << endl;
+}
