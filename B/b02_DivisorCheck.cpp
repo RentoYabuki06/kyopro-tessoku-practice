cb85f404f3877e34baa9f1130a1abdec50206e41
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-08 09:13:49 +0900

    firstadd

---
+++ B/b02_DivisorCheck.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A, B;
+	cin >> A >> B;
+	for (int x = A; x <= B; x++)
+	{
+		if (100 % x == 0)
+		{
+			cout << "Yes" << endl;
+			return 0;
+		}
+	}
+	cout << "No" << endl;
+	return 0;
+}
