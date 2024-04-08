aeb6fec3bbe63ea6f3fe889c706d48db48561fc6
矢吹澪斗 <yabukirento@yabukimiotonoMacBook-Air.local>
2024-04-08 10:58:52 +0900

    firstadd

--- BitExhaustivelySearch/128c_Switches.cpp
+++ BitExhaustivelySearch/128c_Switches.cpp
@@ -2,22 +2,49 @@
 
 using namespace std;
 
-int n, X;
-int a[30];
+int N, M;
+int k[20];
+int s[20][20];
+int p[20];
 
 int main()
 {
-	cin >> n >> X;
-	for (int i = 0; i < n; i++) cin >> a[i];
-	int price = 0;
-	for (int i = n - 1 ; i >= 0; i--)
+	cin >> N >> M;
+	for (int i = 0; i < M; i++)
 	{
-		if (X / (1 << i) == 1)
+		cin >> k[i];
+		for (int j = 0; j < k[i]; j++)
 		{
-			price += a[i];
-			X -= (1 << i);
+			cin >> s[i][j];
 		}
 	}
-	cout << price << endl;
-	return 0;
+	for (int i = 0; i < M; i++) cin >> p[i];
+
+	int count = 0;
+	int flag = 1;
+	for (int i = 0; i < (1 << N); i++)
+	{
+		flag = 1;
+		for (int j = 0; j < M; j++)
+		{
+			int onswitch = 0;
+			for (int l = 0; l < k[j]; l++)
+			{
+				if ((i >> (s[j][l] - 1)) & 1)
+				{
+					onswitch++;
+				}
+			}
+			if (onswitch % 2 != p[j])
+			{
+				flag = -1;
+				break;
+			}
+		}
+		if (flag == 1)
+		{
+			count++;
+		}
+	}
+	cout << count << endl;
 }
